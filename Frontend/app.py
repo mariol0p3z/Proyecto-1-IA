025f97f5220f03403c8abb544aeeebdc96f4c5c6
mariol0p3z <maronlopez60@gmail.com>
2026-04-19 01:06:23 -0600

    Primera version del frontend

---
+++ Frontend/app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+import sys
+import os
+
+#Agregar Backend al path para importar
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Backend'))
+
+from preprocesar import ProcesadorTexto
+
+app = Flask(__name__)
+
+print("Cargando modelo de clasificación...")
+try:
+    models_dir = os.path.join(os.path.dirname(__file__), '..', 'Backend', 'models')
+    modelo = joblib.load(os.path.join(models_dir, 'modelo.pkl'))
+    vocabulario = joblib.load(os.path.join(models_dir, 'vocabulario.pkl'))
+    procesador = joblib.load(os.path.join(models_dir, 'preprocesador.pkl'))
+
+    print(f"Modelo cargado exitosamente")
+    print(f"Clase: {modelo.clases}")
+    print(f"Vocabulario: {len(vocabulario)} palabras")
+except Exception as e:
+    print(f"Error al cargar el modelo: {e}")
+    modelo = None
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    try:
+        #Obtener datos del request
+        data = request.get_json()
+        texto = data.get('text', '')
+
+        if not texto.strip():
+            return jsonify({'error': 'El texto no puede estar vacío'}), 400
+        
+        #Preprocesar el texto
+        tokens = procesador.limpiar_texto(texto)
+
+        #Predecir
+        categoria_pred = modelo.predecir([tokens])[0]
+        probabilidades = modelo.predecir_probabilidades([tokens])[0]
+
+        #Preparar respuesta
+        response = {
+            'categoria': categoria_pred,
+            'confianza': probabilidades[categoria_pred],
+            'probabilidades':{
+                clase: round(prob, 3)
+                for clase, prob in sorted(
+                    probabilidades.items(),
+                    key = lambda x: x[1],
+                    reverse=True
+                )
+            },
+            'tokens_procesados': len(tokens)
+        }
+
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/health')
+def health():
+    if modelo is None:
+        return jsonify({'status': 'error', 'message': 'Modelo no cargado'}), 500
+    return jsonify({
+    'status': 'ok', 
+    'modelo': 'Naive Bayes',       
+    'clases': modelo.clases,
+    'vocabulario': len(vocabulario)        
+    })
+
+if __name__ == '__main__':
+    if modelo is None:
+        print("\n El modelo no se cargo correctamente. Verifique las rutas")
+    
+    print("Servidor flask iniciado")
+    print("URL: http://localhost:5000")
+
+    app.run(debug=True, port=5000)
